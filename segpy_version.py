f98e0c10e41a99cb5126b2b424858386254eefc9
Andrei Voronin <andalevor@gmail.com>
2023-05-17 10:58:15 +0200

    fixed file name

--- segpy_version.py
+++ segpy_version.py
@@ -1,7 +1,7 @@
 from segpy.reader import create_reader
 
 sum = 0.0
-with open('/home/andalevor/Work/UNGF/mozga/vas.stk.segy', 'rb') as segy_in_file:
+with open("filt_mig.sgy", 'rb') as segy_in_file:
     reader = create_reader(segy_in_file)
     for trace_index in reader.trace_indexes():
         for samp in reader.trace_samples(trace_index):
--- segyio_version.py
+++ segyio_version.py
@@ -2,7 +2,7 @@ import segyio
 import numpy as np
 
 sum = 0.0
-with segyio.open('/home/andalevor/Work/UNGF/mozga/vas.stk.segy') as f:
+with segyio.open("filt_mig.sgy") as f:
     for trace in f.trace:
         for samp in trace:
            sum += samp
